393a6fa233036dcd48da377449fadf342771477f
ralu <147080563+raluprodan@users.noreply.github.com>
2025-05-10 16:34:48 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_jHt5L0kQk', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2k9ivj32gs0n0mh7b1f51mt1c0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
